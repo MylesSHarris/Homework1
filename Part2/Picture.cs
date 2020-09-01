e06ccbef1ba2d6f6c1fb4be3fb3e99f99ee0dd7a
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-08-31 18:09:20 -0700

    Homework1

---
+++ Part2/Picture.cs
@@ -0,0 +1,37 @@
+﻿/// Chapter No. 1		Exercise No. 2
+/// File Name: Picture.cs
+/// @author: Myles Harris
+/// Date:  August 24, 2020
+///
+/// Problem Statement: Display a simple picture to the screen
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print the picture to the screen
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Picture
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("                        . - ~ ~ ~ - .                       ");
+			Console.WriteLine("      ..     _      .-~               ~-.                   ");
+			Console.WriteLine("     //|     \\ `..~                      `.                ");
+			Console.WriteLine("    || |      }  }              /       \\  \\              ");
+			Console.WriteLine("(\\   \\ \\~^..'                 |          }  \\           ");
+			Console.WriteLine(" \\`.-~  o      /       }       |        /    \\            ");
+			Console.WriteLine(" (__          |       /        |       /      `.            ");
+			Console.WriteLine("  `- - ~ ~ -._|      /_ - ~ ~ ^|      /- _      `.          ");
+			Console.WriteLine("              |     /          |     /     ~-.     ~- _     ");
+			Console.WriteLine("              |_____|          |_____|         ~ - . _ _~_-_");
+		}
+    }
+}
